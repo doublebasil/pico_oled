51171b39f1c45e50a7c66f1d2b951d8587d3df3c
Tom Flaherty <tom.flaherty03@gmail.com>
2023-11-19 11:11:47 +0000

    Moved manufacturer display init register stuff to a separate function

--- source/oled/oled.cpp
+++ source/oled/oled.cpp
@@ -4,6 +4,7 @@
 #include "pico/binary_info.h"
 #include "hardware/spi.h"
 
+static inline void m_displayInit( void );
 static inline void m_chipSelect( void );
 static inline void m_chipDeselect( void );
 static inline void m_writeReg( uint8_t reg );
@@ -53,18 +54,21 @@ int oled_init( int8_t dinPin, int8_t clkPin, int8_t csPin, int8_t dcPin,
     {
         spi_init( spi0, baudrate );
         m_spiInstance = 0;
-        // Set the SPI format to be the same as Arduino's SPI Mode 3, i.e.:
-        // Most significant bit first, clock polarity = 1, clock phase = 1
-        // spi_set_format( spi0, 8U, SPI_CPOL_1, SPI_CPHA_1, SPI_MSB_FIRST );
+        /* Set the SPI format to be the same as Arduino's SPI Mode 3, i.e.:
+         * Most significant bit first, clock polarity = 1, clock phase = 1
+         * This line is not needed, but it's good to ensure the correct settings */
+        spi_set_format( spi0, 8U, SPI_CPOL_1, SPI_CPHA_1, SPI_MSB_FIRST );
     }
     else if( ( spiOutput == 1 ) &&
         ( ( dinPin == 11 ) || ( dinPin == 15 ) ) && // Check din pin is connected to a SPI1 TX
         ( ( clkPin == 10 ) || ( clkPin == 14 ) ) )  // Check clk pin is connected to a SPI1 SCK
     {
         spi_init( spi1, baudrate );
+        // m_spiInstance is so that 
         m_spiInstance = 1;
-        // Set the SPI format to be the same as Arduino's SPI Mode 3, i.e.:
-        // Most significant bit first, clock polarity = 1, clock phase = 1
+        /* Set the SPI format to be the same as Arduino's SPI Mode 3, i.e.:
+         * Most significant bit first, clock polarity = 1, clock phase = 1
+         * This line is not needed, but it's good to ensure the correct settings */
         spi_set_format( spi1, 8U, SPI_CPOL_1, SPI_CPHA_1, SPI_MSB_FIRST );
     }
     else
@@ -74,80 +78,14 @@ int oled_init( int8_t dinPin, int8_t clkPin, int8_t csPin, int8_t dcPin,
     }
 
     gpio_put( m_rstPin, 1 );
-    sleep_ms( 100U ); // This can probably be changed
+    sleep_ms( 100U );
     gpio_put( m_rstPin, 0 );
     sleep_ms( 100U );
     gpio_put( m_rstPin, 1 );
 
-
-    // Now for some inexplicable magic from the manufacturer. Comments here are from the manufacturer code
-    m_writeReg(0xFD);  // Command lock
-    m_writeData(0x12);
-    m_writeReg(0xFD);  // Command lock
-    m_writeData(0xB1);
-
-    m_writeReg(0xAE);  // Display off
-    m_writeReg(0xA4);  // Normal Display mode
-
-    m_writeReg(0x15);  // Set column address
-    m_writeData(0x00); // Column address start 00
-    m_writeData(0x7F); // Column address end 127
-    m_writeReg(0x75);  // Set row address
-    m_writeData(0x00); // Row address start 00
-    m_writeData(0x7F); // Row address end 127    
-
-    m_writeReg(0xB3);
-    m_writeData(0xF1);
-
-    m_writeReg(0xCA);  
-    m_writeData(0x7F);
-
-    m_writeReg(0xA0);  // Set re-map & data format
-    m_writeData(0x74); // Horizontal address increment
-
-    m_writeReg(0xA1);  // Set display start line
-    m_writeData(0x00); // Start 00 line
-
-    m_writeReg(0xA2);  // Set display offset
-    m_writeData(0x00);
-
-    m_writeReg(0xAB);  
-    m_writeReg(0x01);  
-
-    m_writeReg(0xB4);  
-    m_writeData(0xA0);   
-    m_writeData(0xB5);  
-    m_writeData(0x55);    
-
-    m_writeReg(0xC1);  
-    m_writeData(0xC8); 
-    m_writeData(0x80);
-    m_writeData(0xC0);
-
-    m_writeReg(0xC7);  
-    m_writeData(0x0F);
-
-    m_writeReg(0xB1);  
-    m_writeData(0x32);
-
-    m_writeReg(0xB2);  
-    m_writeData(0xA4);
-    m_writeData(0x00);
-    m_writeData(0x00);
-
-    m_writeReg(0xBB);  
-    m_writeData(0x17);
-
-    m_writeReg(0xB6);
-    m_writeData(0x01);
-
-    m_writeReg(0xBE);
-    m_writeData(0x05);
-
-    m_writeReg(0xA6);
+    m_displayInit();
 
     sleep_ms( 200U );
-    // End of inexplicable code
 
     // Turn on the display
     m_writeReg(0xAF);
@@ -213,6 +151,75 @@ void oled_test( void )
             drawMode = 0;
     }
     
+}
+
+static inline void m_displayInit( void )
+{
+    // Now for some inexplicable magic from the manufacturer. Comments here are from the manufacturer
+    m_writeReg(0xFD);  // Command lock
+    m_writeData(0x12);
+    m_writeReg(0xFD);  // Command lock
+    m_writeData(0xB1);
+
+    m_writeReg(0xAE);  // Display off
+    m_writeReg(0xA4);  // Normal Display mode
+
+    m_writeReg(0x15);  // Set column address
+    m_writeData(0x00); // Column address start 00
+    m_writeData(0x7F); // Column address end 127
+    m_writeReg(0x75);  // Set row address
+    m_writeData(0x00); // Row address start 00
+    m_writeData(0x7F); // Row address end 127    
+
+    m_writeReg(0xB3);
+    m_writeData(0xF1);
+
+    m_writeReg(0xCA);  
+    m_writeData(0x7F);
+
+    m_writeReg(0xA0);  // Set re-map & data format
+    m_writeData(0x74); // Horizontal address increment
+
+    m_writeReg(0xA1);  // Set display start line
+    m_writeData(0x00); // Start 00 line
+
+    m_writeReg(0xA2);  // Set display offset
+    m_writeData(0x00);
+
+    m_writeReg(0xAB);  
+    m_writeReg(0x01);  
+
+    m_writeReg(0xB4);  
+    m_writeData(0xA0);   
+    m_writeData(0xB5);  
+    m_writeData(0x55);    
+
+    m_writeReg(0xC1);  
+    m_writeData(0xC8); 
+    m_writeData(0x80);
+    m_writeData(0xC0);
+
+    m_writeReg(0xC7);  
+    m_writeData(0x0F);
+
+    m_writeReg(0xB1);  
+    m_writeData(0x32);
+
+    m_writeReg(0xB2);  
+    m_writeData(0xA4);
+    m_writeData(0x00);
+    m_writeData(0x00);
+
+    m_writeReg(0xBB);  
+    m_writeData(0x17);
+
+    m_writeReg(0xB6);
+    m_writeData(0x01);
+
+    m_writeReg(0xBE);
+    m_writeData(0x05);
+
+    m_writeReg(0xA6);
 }
 
 static inline void m_chipSelect( void )
